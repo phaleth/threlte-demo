68edc85e1c765f82222cfda2b592c21cd3cea2a1
phaleth <29838606+phaleth@users.noreply.github.com>
2023-05-02 18:57:42 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { Canvas } from '@threlte/core';
+	import Scene from '../components/Scene.svelte';
+</script>
+
+<div class="container">
+	<Canvas>
+		<Scene />
+	</Canvas>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+</style>
